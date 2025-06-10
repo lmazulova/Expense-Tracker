5e2e915a87eb602f88de21f6816002cbc72058be
Elizaveta Mazulova <lmazulova@bk.ru>
2025-06-10 21:26:06 +0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by user on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
